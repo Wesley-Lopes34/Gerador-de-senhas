3e0ce36488f493754d70b16328089a4550698d03
Wesley Lopes <mikemagma666@gmail.com>
2023-11-07 08:51:17 -0300

    adição de cometarios para melhor entendimento do código

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Criação de botões e o medidor do tamanho da senha
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
@@ -6,15 +7,18 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password")
 
+//nessa parte eu estou especificando todos os caracteres que é possivel uma senha receber
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
+//Função que o slider recebe o valor que for selecionado nele
 slider.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
 
+//Função que gera a senha, nessa função eu começo com um contador que recebe um caractere aleatorio dentre todos aqueles que eu passei e ele atribui esse valor a outro e vai buscando varios valores até formar uma senha.
 function generatePassword(){
     
     let pass = "";
@@ -29,6 +33,7 @@ function generatePassword(){
 
 }
 
+//Função para copiar a senha
 function copyPassword(){
     alert("Copiada com sucesso")
     navigator.clipboard.writeText(novaSenha);
